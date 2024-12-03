2a843fc49f39f4cde8e651215cc3098eae843b96
Adam-KR123 <128384355+Adam-KR123@users.noreply.github.com>
2024-12-03 22:00:22 +0100

    uprgrade

--- scripts/main.js
+++
@@ -1,7 +0,0 @@
-window.scrollTo(0, 0);
-document.body.style.overflow="hidden";
-loading = setTimeout(()=>{
-    document.querySelector(".load").style.display="none"
-    document.body.style.overflow="auto";
-
-},5000)
